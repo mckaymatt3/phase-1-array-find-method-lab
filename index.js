6cea9d56771737059a7d3bdf641e1e39dc2827eb
mckaymatt3 <mckaymatt@outlook.com>
2022-02-03 17:24:34 -0500

    Completed Message

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+
+function superbowlWin(record) {
+    let checkYear = record.find((record =>
+        record.result === 'W'));
+      if (checkYear) {
+        return checkYear.year
+      }
+      else return undefined
+}
