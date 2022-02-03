a10974d8d41f045d5b78a3e26fd3d5f829b4bae5
mckaymatt3 <mckaymatt@outlook.com>
2022-02-03 17:23:47 -0500

    commit tree 569e99e00249703b3052c16c30ea143cfb0ca410

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // code your solution here
+
+// FIRST ATTEMPT - BASIC
+// const result1 = record.find(superbowlWin)
+
+// function superbowlWin(date) {
+//   return date.result === 'W'
+// }
+
+// console.log(result1)
+
+
+// SECOND ATTEMPT - INCLUDING IF ELSE
+// const result1 = record.find(superbowlWin)
+
+// function superbowlWin(date) {
+//     let date1 = [];
+//     date1.push(date)
+//     if (record.result ==='W') {
+//       return date1
+//     }
+//     else {
+//       return undefined
+//     }
+    
+// }
+
+// THIRD ATTEMPT - BASIC ONCE AGAIN
+
+function superbowlWin(record) {
+    let checkYear = record.find((record =>
+        record.result === 'W'));
+      if (checkYear) {
+        return checkYear.year
+      }
+      else return undefined
+  }
